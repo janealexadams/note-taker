203bfc82c9e79051ee651c0525ec4a610ea9dbc7
janealexadams <janealexadams@gmail.com>
2023-10-12 19:54:26 -0700

    added get and post routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
 const path = require('path');
+const {readFromFile, writeToFile} = require('../helpers/fsUtils');
 
-// GET Route for retrieving index.html
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+// /api endpoint.
+
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/notes', async (req, res) => {
+    let notes = await readFromFile('./db/db.json');
+    let parseNotes = JSON.parse(notes);
+    res.json(parseNotes);
 });
 
 // GET Route for retrieving notes.html
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+router.post('/notes', async (req, res) => {
+    let notes = await readFromFile('./db/db.json');
+    let parseNotes = JSON.parse(notes);
+    const {title,text} = req.body;
+    const newNote = {title,text}
+
+    parseNotes.push(newNote)
+    const updatedNotes = await writeToFile('./db/db.json', JSON.stringify(parseNotes))
+    res.json(updatedNotes);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+
+// Import the notes router
+const api = require('./routes/notes.js'); 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
 app.use(express.static('public'));
+
+// Send all the requests that begin with /api to the index.js in the routes folder
+app.use('/api', api);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
