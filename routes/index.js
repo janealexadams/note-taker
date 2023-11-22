4f6d9e2a6d1d923768b8ee4af9be1f7858f0b2b7
janealexadams <janealexadams@gmail.com>
2023-11-21 17:37:21 -0800

    updated post route

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,52 @@
 const router = require('express').Router();
-const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const {readFromFile, writeToFile} = require('../helpers/fsUtils');
 
 
 // GET /api/notes will read the db.json file and return all saved notes as JSON.
-router.get('/notes', async (req, res) => {
-    let notes = await readFromFile('./db/db.json');
-    let parseNotes = JSON.parse(notes);
-    res.json(parseNotes);
-});
+// router.get('/notes', async (req, res) => {
+//     let notes = await readFromFile('./db/db.json');
+//     let parseNotes = JSON.parse(notes);
+//     res.json(parseNotes);
+// });
 
-
-// GET route to returns a specific note
-router.get('/api/notes/:id', (req, res) => {
-    const requestedNote = req.params.id();
-  
-    // Iterate through the note id to check if it matches `req.params.id`
-    for (let i = 0; i < termData.length; i++) {
-      if (requestedNote === parseNotes[i].id()) {
-        return res.json(parseNotes[i]);
-      }
-    }
-  
-    // Return a message if the id doesn't exist in our DB
-    return res.json('No note found with that id');
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-// GET route for retrieving notes.html.
+// GET route to return a specific note
+router.get('/notes/:id', (req, res) => {
+    const requestedNote = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === requestedNote);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  
+    // // Iterate through the note id to check if it matches `req.params.id`
+    // for (let i = 0; i < termData.length; i++) {
+    //   if (requestedNote === parseNotes[i].id()) {
+    //     return res.json(parseNotes[i]);
+    //   }
+    // }
+  
+    // // Return a message if the id doesn't exist in our DB
+    // return res.json('No note found with that id');
+  });
+
+// POST route for a new tip
 router.post('/notes', async (req, res) => {
     let notes = await readFromFile('./db/db.json');
     let parseNotes = JSON.parse(notes);
     const {title,text} = req.body;
-    const newNote = {title,text}
+    const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+    };
 
     parseNotes.push(newNote)
     const updatedNotes = await writeToFile('./db/db.json', JSON.stringify(parseNotes))
@@ -40,5 +55,21 @@ router.post('/notes', async (req, res) => {
 
 
 // DELETE Route 
+router.delete('notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((parseNotes) => JSON.parse(parseNotes))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Note has been deleted 🗑️`);
+      });
+  });
+
 
 module.exports = router;
