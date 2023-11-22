a1b05d599826f410edbfa708f96f01f3e0250894
janealexadams <janealexadams@gmail.com>
2023-11-21 16:46:46 -0800

    updated get route with id parameter

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,31 @@ const router = require('express').Router();
 const path = require('path');
 const {readFromFile, writeToFile} = require('../helpers/fsUtils');
 
-// /api endpoint.
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// GET /api/notes will read the db.json file and return all saved notes as JSON.
 router.get('/notes', async (req, res) => {
     let notes = await readFromFile('./db/db.json');
     let parseNotes = JSON.parse(notes);
     res.json(parseNotes);
 });
 
-// GET Route for retrieving notes.html.
+
+// GET route to returns a specific note
+router.get('/api/notes/:id', (req, res) => {
+    const requestedNote = req.params.id();
+  
+    // Iterate through the note id to check if it matches `req.params.id`
+    for (let i = 0; i < termData.length; i++) {
+      if (requestedNote === parseNotes[i].id()) {
+        return res.json(parseNotes[i]);
+      }
+    }
+  
+    // Return a message if the id doesn't exist in our DB
+    return res.json('No note found with that id');
+  });
+
+// GET route for retrieving notes.html.
 router.post('/notes', async (req, res) => {
     let notes = await readFromFile('./db/db.json');
     let parseNotes = JSON.parse(notes);
